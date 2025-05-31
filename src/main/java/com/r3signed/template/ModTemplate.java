90198f09a981423be27d7661190591c4662c88ee
r3signed <alexsinclaire1205@gmail.com>
2025-05-31 17:34:16 +0100

    Initial commit

---
+++ src/main/java/com/r3signed/template/ModTemplate.java
@@ -0,0 +1,10 @@
+package com.r3signed.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModTemplate implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/r3signed/template/client/ModTemplateClient.java
@@ -0,0 +1,10 @@
+package com.r3signed.template.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTemplateClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
